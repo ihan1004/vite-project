6829f3b8ef5a02cac7c05048004b8dfa9b978776
[Lee EunSeo] <dldmstj987@naver.com>
2025-02-03 13:46:13 +0900

    짧은 글 등록(input), 삭제 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   let [따봉, 따봉변경] = useState([0,0,0]); //좋아요 useState
   let [modal, setModal] = useState(false); // 페이지 로드 시 모달창이 닫혀있는 상태
   let [modalTilte, setModalTitle] = useState(0); //모달창의 제목을 바꾸기 위한 state
-
+  let [입력값, 입력값변경] = useState(''); //입력값을 받아오기 위한 state
 
   return (
     
@@ -43,20 +43,34 @@ function App() {
           <h4 onClick={() => { 
             setModal(!modal); setModalTitle(i); }}>
               { 글제목[i] }  
-            <span onClick={ () => {
+            <span onClick={ (e) => { e.stopPropagation(); // 이벤트 버블링을 막아줌
                let copy = [...따봉]; // 따봉 배열을 복사
                copy[i] = copy[i] + 1; // 복사한 배열의 i번째 요소에 +1
                따봉변경(copy); // 상태 업데이트
                }}>👍</span>{ 따봉[i] }
           </h4>
           <p>2월 17일 발행</p>
+          <button onClick={() => {
+            let copy = [...글제목];
+            copy.splice(i, 1); //splice는 배열의 i번째 요소부터 1개를 삭제
+            글제목변경(copy);
+          }}>삭제</button>
         </div>
         )
       })
       }
+
+      <input  onChange={(e) => {입력값변경(e.target.value); console.log(입력값)}}/>
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy.unshift(입력값); //unshift는 배열의 맨 앞에 추가
+        글제목변경(copy);
+      }}>등록</button>
+
+{/* 
       <button onClick={() => { setModalTitle(0)}}>글제목0</button>
       <button onClick={() => { setModalTitle(1)}}>글제목1</button>
-      <button onClick={() => { setModalTitle(2)}}>글제목2</button>
+      <button onClick={() => { setModalTitle(2)}}>글제목2</button> */}
      {
         modal == true ? <Modal modalTilte={modalTilte} 글제목={글제목}/> : null
       }
